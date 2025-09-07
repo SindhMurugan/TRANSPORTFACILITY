826b9962270f52e18acb77244a704d331205031f
SindhMurugan <sindhu3100@gmail.com>
2025-09-07 23:14:10 +0530

    transportfacility

--- src/app/add-new-ride/add-new-ride.component.ts
+++ src/app/add-new-ride/add-new-ride.component.ts
@@ -1,54 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { RideDataModel } from '../data-model.interface';
 import { ServiceService } from '../shareData/service.service';
 
 @Component({
   selector: 'app-add-new-ride',
   templateUrl: './add-new-ride.component.html',
-  styleUrls: ['./add-new-ride.component.css']
+  styleUrls: ['./add-new-ride.component.css'],
 })
 export class AddNewRideComponent {
+  addNewRideForm: FormGroup;
+  newRide: Record<string, string> = {};
+  restoreRideDataFromStorage: RideDataModel[] = new Array();
 
-  addNewRideForm:FormGroup
-  newRide:Record<string,string> ={}
-  restoreRideDataFromStorage:RideDataModel[] = new Array()
-
-  constructor(private fb:FormBuilder,private service:ServiceService) { 
-    this.addNewRideForm=this.fb.group({
-      employee_id:['',Validators.required],
-      vehicle_type:[''],
-      vehicle_No:['',Validators.required],
-      vacant_Seat:['',[Validators.required,Validators.minLength]],
-      time:['',Validators.required],
-      pickup_Point:['',Validators.required],
-      destination:['',Validators.required],
-
-
-    })
+  constructor(private fb: FormBuilder, private service: ServiceService) {
+    this.addNewRideForm = this.fb.group({
+      employee_id: ['', Validators.required],
+      vehicle_type: [''],
+      vehicle_No: ['', [Validators.required, Validators.minLength(4)]],
+      vacant_Seat: ['', Validators.required],
+      time: ['', Validators.required],
+      pickup_Point: ['', Validators.required],
+      destination: ['', Validators.required],
+    });
   }
 
-
-
-  get getFormControls(){
+  get getFormControls() {
     return this.addNewRideForm.controls;
   }
 
-
-  addedNewRide(){
-    console.log('this.addNewRideForm' , this.addNewRideForm)
-    const active_employeeId = this.addNewRideForm.get('employee_id')?.value
-    const checkEmployeeId = this.service.getValueFromLocalStorage.some((ride:RideDataModel) => ride.employee_id == active_employeeId )
-    if(checkEmployeeId){
-      alert("This employee id is already exits")
-      return 
+  addedNewRide() {
+    const active_employeeId = this.addNewRideForm.get('employee_id')?.value;
+    const checkEmployeeId = this.service.getValueFromLocalStorage.some(
+      (ride: RideDataModel) => ride.employee_id == active_employeeId
+    );
+    if (checkEmployeeId) {
+      alert('This employee id is already exits');
+      return;
     }
-     
-  
-    this.newRide = this.addNewRideForm.value 
-    this.addNewRideForm.reset()
-    console.log(this.addNewRideForm.value , 'mmmm')
 
+    this.newRide = this.addNewRideForm.value;
+    this.addNewRideForm.reset();
   }
-
 }
--- src/app/ride-list/ride-list.component.ts
+++ src/app/ride-list/ride-list.component.ts
@@ -6,70 +6,55 @@ import { RideDataModel } from '../data-model.interface';
   selector: 'app-ride-list',
   templateUrl: './ride-list.component.html',
   styleUrls: ['./ride-list.component.css'],
- 
 })
-export class RideListComponent implements OnChanges,OnInit {
+export class RideListComponent implements OnChanges, OnInit {
+  @Input() newRideData: Record<string, string> = {};
+  restoreRideDataFromStorage: RideDataModel[] = new Array();
+  current_time = new Date();
+  userInput: string = '';
+  activeUser: number = 0;
 
-  @Input() newRideData:Record<string,string> ={}
-  restoreRideDataFromStorage:RideDataModel[] = new Array()
-  current_time = new Date()
-  userInput:string=''
-  activeUser:number = 0
+  constructor(private service: ServiceService) {}
 
-  constructor(private service:ServiceService) { }
-
-  ngOnChanges(changes:any): void {
-
-    if(changes.newRideData.previousValue){
-      let  existingData:any = this.service.getValueFromLocalStorage
-      existingData.push(changes.newRideData.currentValue)
-      this.activeUser = changes.newRideData.currentValue.employee_id
-      this.service.setValueToLocalStorage(existingData)
+  ngOnChanges(changes: any): void {
+    if (changes.newRideData.previousValue) {
+      let existingData: any = this.service.getValueFromLocalStorage;
+      existingData.push(changes.newRideData.currentValue);
+      this.activeUser = changes.newRideData.currentValue.employee_id;
+      this.service.setValueToLocalStorage(existingData);
     }
-    this.restoreRideDataFromStorage= this.service.getValueFromLocalStorage
-   
-    
-    
+    this.restoreRideDataFromStorage = this.service.getValueFromLocalStorage;
   }
 
   ngOnInit(): void {
-    this.restoreRideDataFromStorage= this.service.getValueFromLocalStorage
+    this.restoreRideDataFromStorage = this.service.getValueFromLocalStorage;
   }
 
-  trackById(index:number , item:any){
-    return item.id
-
+  trackById(index: number, item: any) {
+    return item.id;
   }
 
-
-
-
-  rideBooked(rideDetails:RideDataModel):void{
-   
-
-    if(rideDetails.employee_id == this.activeUser){
-      alert("Sorry , you can't book the ride twise")
+  rideBooked(rideDetails: RideDataModel): void {
+    if (rideDetails.employee_id == this.activeUser) {
+      alert("Sorry , you can't book the ride twise");
       return;
     }
 
-    if(rideDetails['vacant_Seat'] > 0){
-      let bookedRide:RideDataModel = {...rideDetails , vacant_Seat:rideDetails['vacant_Seat']-1  }
-     
+    if (rideDetails['vacant_Seat'] > 0) {
+      let bookedRide: RideDataModel = {
+        ...rideDetails,
+        vacant_Seat: rideDetails['vacant_Seat'] - 1,
+      };
 
-      this.restoreRideDataFromStorage.forEach((ride)=> {
-        if(ride.employee_id == rideDetails.employee_id){
-          let index = this.restoreRideDataFromStorage.indexOf(ride)
-           this.restoreRideDataFromStorage[index] = bookedRide
-           
+      this.restoreRideDataFromStorage.forEach((ride) => {
+        if (ride.employee_id == rideDetails.employee_id) {
+          let index = this.restoreRideDataFromStorage.indexOf(ride);
+          this.restoreRideDataFromStorage[index] = bookedRide;
         }
-      })
+      });
 
-      this.service.setValueToLocalStorage(this.restoreRideDataFromStorage)
-      this.restoreRideDataFromStorage= this.service.getValueFromLocalStorage
+      this.service.setValueToLocalStorage(this.restoreRideDataFromStorage);
+      this.restoreRideDataFromStorage = this.service.getValueFromLocalStorage;
     }
-   
-    
-
   }
-
 }
